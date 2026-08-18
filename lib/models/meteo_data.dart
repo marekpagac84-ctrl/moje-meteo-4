068e36673ee8228e4f3608fd5389c064b2d15276
marekpagac84-ctrl <marekpagac84@gmail.com>
2026-08-18 12:47:06 +0200

    Add BarometerState, PresetLocation, and CommunityMarker classes
    Added BarometerState, PresetLocation, and CommunityMarker classes with properties and methods.

--- lib/models/meteo_data.dart
+++ lib/models/meteo_data.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class MeteoApiData {
   final double currentTemp;
   final double currentPressure;
@@ -40,4 +42,64 @@ class MeteoApiData {
       hourlyWeatherCodes: codes,
     );
   }
+}
+
+class BarometerState {
+  final double currentPressure;
+  final double pressureChangeRate;
+  final bool isMovingVertically;
+  final List<double> pressureHistory;
+  final double estimatedAltitude;
+  final double basePressure;
+
+  BarometerState({
+    required this.currentPressure,
+    required this.pressureChangeRate,
+    required this.isMovingVertically,
+    required this.pressureHistory,
+    required this.estimatedAltitude,
+    required this.basePressure,
+  });
+
+  BarometerState copyWith({
+    double? currentPressure,
+    double? pressureChangeRate,
+    bool? isMovingVertically,
+    List<double>? pressureHistory,
+    double? estimatedAltitude,
+    double? basePressure,
+  }) {
+    return BarometerState(
+      currentPressure: currentPressure ?? this.currentPressure,
+      pressureChangeRate: pressureChangeRate ?? this.pressureChangeRate,
+      isMovingVertically: isMovingVertically ?? this.isMovingVertically,
+      pressureHistory: pressureHistory ?? this.pressureHistory,
+      estimatedAltitude: estimatedAltitude ?? this.estimatedAltitude,
+      basePressure: basePressure ?? this.basePressure,
+    );
+  }
+}
+
+class PresetLocation {
+  final String name;
+  final double lat;
+  final double lng;
+
+  const PresetLocation({
+    required this.name,
+    required this.lat,
+    required this.lng,
+  });
+}
+
+class CommunityMarker {
+  final String title;
+  final double lat;
+  final double lng;
+
+  CommunityMarker({
+    required this.title,
+    required this.lat,
+    required this.lng,
+  });
 }
