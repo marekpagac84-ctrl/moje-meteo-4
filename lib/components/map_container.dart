141ddc22d19a836b1e92a3ef51c0cb6be90b41fa
marekpagac84-ctrl <marekpagac84@gmail.com>
2026-08-18 12:47:33 +0200

    Refactor MapContainer to use FlutterMap
    Refactor MapContainer from StatefulWidget to StatelessWidget. Replace WebView with FlutterMap for displaying map and user location.

--- lib/components/map_container.dart
+++ lib/components/map_container.dart
@@ -1,44 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class MapContainer extends StatefulWidget {
+class MapContainer extends StatelessWidget {
   final LatLng userLocation;
+  final bool showRadarOverlay;
+  final Function(LatLng) onLocationSelected;
 
   const MapContainer({
     super.key,
     required this.userLocation,
+    required this.showRadarOverlay,
+    required this.onLocationSelected,
   });
-
-  @override
-  State<MapContainer> createState() => _MapContainerState();
-}
-
-class _MapContainerState extends State<MapContainer> {
-  late final WebViewController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _initWebView();
-  }
-
-  @override
-  void didUpdateWidget(covariant MapContainer oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.userLocation != widget.userLocation) {
-      _initWebView();
-    }
-  }
-
-  void _initWebView() {
-    final String url =
-        'https://embed.windy.com/embed2.html?lat=${widget.userLocation.latitude}&lon=${widget.userLocation.longitude}&detailLat=${widget.userLocation.latitude}&detailLon=${widget.userLocation.longitude}&width=650&height=450&zoom=8&level=surface&overlay=radar&product=radar&menu=&message=&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1';
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(url));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +20,57 @@ class _MapContainerState extends State<MapContainer> {
       height: 380,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
-        child: WebViewWidget(controller: _controller),
+        child: Stack(
+          children: [
+            FlutterMap(
+              options: MapOptions(
+                initialCenter: userLocation,
+                initialZoom: 7.0,
+                maxZoom: 10.0,
+                minZoom: 3.0,
+                onTap: (tapPosition, point) {
+                  onLocationSelected(point);
+                },
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.meteo_app',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: userLocation,
+                      child: const Icon(Icons.location_pin, color: Colors.red, size: 36),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Positioned(
+              top: 12,
+              right: 12,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.75),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.map, color: Colors.lightBlueAccent, size: 16),
+                    SizedBox(width: 6),
+                    Text(
+                      'Meteo Mapa',
+                      style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
