f2cbf5f7106b9b15848b5bc6b2e388f382dd6e00
marekpagac84-ctrl <marekpagac84@gmail.com>
2026-08-19 17:13:55 +0200

    Refactor WindyMapContainer to use lat/lng parameters
    Refactor WindyMapContainer to use latitude and longitude directly instead of LatLng. Update the WebView loading logic to respond to changes in latitude and longitude.

--- lib/components/windy_map_container.dart
+++ lib/components/windy_map_container.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WindyMapContainer extends StatefulWidget {
-  final LatLng userLocation;
+  final double lat;
+  final double lng;
 
   const WindyMapContainer({
     super.key,
-    required this.userLocation,
+    required this.lat,
+    required this.lng,
   });
 
   @override
@@ -16,71 +17,33 @@ class WindyMapContainer extends StatefulWidget {
 
 class _WindyMapContainerState extends State<WindyMapContainer> {
   late final WebViewController _controller;
-  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-
-    final double lat = widget.userLocation.latitude;
-    final double lng = widget.userLocation.longitude;
-
-    final String windyUrl =
-        'https://embed.windy.com/embed2.html?lat=$lat&lon=$lng&detailLat=$lat&detailLon=$lng&width=650&height=450&zoom=8&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=m%2Fs&metricTemp=%C2%B0C&radarRange=-1';
-
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFF0F172A))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            if (mounted) {
-              setState(() {
-                _isLoading = true;
-              });
-            }
-          },
-          onPageFinished: (String url) {
-            if (mounted) {
-              setState(() {
-                _isLoading = false;
-              });
-            }
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('Windy WebView error: ${error.description}');
-          },
+      ..loadRequest(
+        Uri.parse(
+          'https://embed.windy.com/embed2.html?lat=${widget.lat}&lon=${widget.lng}&zoom=8&level=surface&overlay=wind&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=m%2Fs&metricTemp=%C2%B0C&radarRange=-1',
         ),
-      )
-      ..loadRequest(Uri.parse(windyUrl));
+      );
+  }
+
+  @override
+  void didUpdateWidget(covariant WindyMapContainer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.lat != widget.lat || oldWidget.lng != widget.lng) {
+      _controller.loadRequest(
+        Uri.parse(
+          'https://embed.windy.com/embed2.html?lat=${widget.lat}&lon=${widget.lng}&zoom=8&level=surface&overlay=wind&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=m%2Fs&metricTemp=%C2%B0C&radarRange=-1',
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 380,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color(0xFF1E293B),
-        border: Border.all(
-          color: Colors.white.withOpacity(0.1),
-          width: 1,
-        ),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: Stack(
-          children: [
-            WebViewWidget(controller: _controller),
-            if (_isLoading)
-              const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.cyanAccent,
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
+    return WebViewWidget(controller: _controller);
   }
 }
