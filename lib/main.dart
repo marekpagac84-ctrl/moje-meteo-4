47944d67a21f0c04d366dccd76f447fced6074f3
marekpagac84-ctrl <marekpagac84@gmail.com>
2026-09-17 18:23:27 +0200

    Integrate Android Storm Orb widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:sensors_plus/sensors_plus.dart';
@@ -60,6 +61,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  static const MethodChannel _widgetChannel = MethodChannel('moje_meteo/widget');
   // ==========================================================
   // NASTAVENIA
   // ==========================================================
@@ -217,6 +219,8 @@ class _MainScreenState extends State<MainScreen> {
             _lng = position.longitude;
             _locationName = 'Moja GPS poloha';
           });
+
+          await _syncHomeWidgetLocation();
         }
       }
     } catch (e) {
@@ -224,6 +228,18 @@ class _MainScreenState extends State<MainScreen> {
     }
 
     await _fetchWeatherData();
+  }
+
+  Future<void> _syncHomeWidgetLocation() async {
+    try {
+      await _widgetChannel.invokeMethod('updateLocation', {
+        'lat': _lat,
+        'lng': _lng,
+        'name': _locationName,
+      });
+    } catch (e) {
+      debugPrint('Widget location sync error: $e');
+    }
   }
 
   // ==========================================================
@@ -927,6 +943,7 @@ class _MainScreenState extends State<MainScreen> {
                     _locationName = preset.name;
                   });
 
+                  _syncHomeWidgetLocation();
                   _fetchWeatherData();
                 },
                 onUseGps: _initGpsLocation,
